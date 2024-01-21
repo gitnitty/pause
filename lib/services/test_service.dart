7ce48fb05b15d51543c21eaa0d4ab31f099a251d
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-01-21 21:19:07 +0900

    main branch

--- lib/services/test_service.dart
+++ lib/services/test_service.dart
@@ -11,7 +11,7 @@ class TestService {
 
   static Future<bool> create(Map<String, dynamic> data) async {
     try {
-      await FirebaseService.fireStore.collection("컬렉션").add(data);
+      await FirebaseService.fireStore.collection("User").add(data);
       await FirebaseService.fireStore.collection("컬렉션").doc("id").set(data);
       return true;
     } catch (e) {
@@ -21,9 +21,20 @@ class TestService {
 
   static Future<bool> read(String id) async {
     try {
-      final snapshot = await FirebaseService.fireStore.collection("컬렉션").doc(id).get();
+      final snapshot =
+          await FirebaseService.fireStore.collection("컬렉션").doc(id).get();
+      final snapshot2 = await FirebaseService.fireStore
+          .collection("목표")
+          .where("작성자", isEqualTo: "abc")
+          .where("작성일", isLessThanOrEqualTo: "2024 1 17")
+          .where("작성일", isGreaterThanOrEqualTo: "2024 1 16")
+          .get();
+      // backend http.get post("")
       String data = snapshot.get("field");
-      Map<String,dynamic>? dataMap = snapshot.data();
+      Map<String, dynamic>? dataMap = snapshot.data();
+      DateTime nowDate = DateTime.now();
+      DateTime(nowDate.year,nowDate.month+1,1).difference(DateTime(nowDate.year,nowDate.month,1)).inDays;
+      nowDate.add(Duration(days: 1));
       return true;
     } catch (e) {
       return false;
@@ -34,7 +45,7 @@ class TestService {
     try {
       // name : a -> b 로 바꿀때
       await FirebaseService.fireStore.collection("컬렉션").doc(id).update({
-        "name" : "b",
+        "name": "b",
       });
       return true;
     } catch (e) {
